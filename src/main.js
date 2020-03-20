0490d48c30a3ffde3e26719d919a766b49824eca
Daniel Acosta <d4ni3L_15@hotmail.com>
2020-03-20 11:10:58 -0500

    instalacion de vue-bootstrap y axios

--- src/main.js
+++ src/main.js
@@ -3,6 +3,20 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import BootstrapVue from 'bootstrap-vue'
+
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(BootstrapVue)
+Vue.use(VueAxios, axios)
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Agregamos la URL base de nuestra API
+axios.defaults.baseURL = 'http://localhost:4000/api';
+
 Vue.config.productionTip = false
 
 new Vue({
